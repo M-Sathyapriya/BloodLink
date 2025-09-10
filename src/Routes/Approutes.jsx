46bb3d683fa59c0cee6190446328d1ff62221f94
Vishali-Vivek006 <vishalivanith2006@gmail.com>
2025-09-10 21:22:58 +0530

    conflict

--- src/Routes/Approutes.jsx
+++ src/Routes/Approutes.jsx
@@ -6,11 +6,9 @@ import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import RegistrationForm2 from '../components/Registeration2';
 import RegistrationForm3 from '../components/Registration3';
-<<<<<<< HEAD
 import ResetPassword from '../components/ResetPassword';
 import OtpVerify from '../components/otpVerify';
 import ForgotPassword from '../components/ForgotPassword';
-=======
 import KGHospitalInfo from '../components/view';
 import IMA from '../components/view2';
 import Kuppu from '../components/view3';
@@ -19,7 +17,6 @@ import Devihospital from '../components/viewh1';
 import Corporation from '../components/viewh2';
 import Port from '../components/viewh3';
 import Hospital from '../Pages/Hospital/Hospital';
->>>>>>> Vishali
 
 function AppRoutes() {
   return (
@@ -30,12 +27,10 @@ function AppRoutes() {
         <Route path='/RegistrationForm1' element={<RegistrationForm1 />} />
         <Route path='/RegistrationForm2' element={<RegistrationForm2 />} />
         <Route path='/RegistrationForm3' element={<RegistrationForm3 />} />
-<<<<<<< HEAD
-        //<Route path='/resetpassword' element={<ResetPassword/>}/>
-        //<Route path='/verifyotp' element={<OtpVerify/>}/>
-        //<Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path='/resetpassword' element={<ResetPassword/>}/>
+        <Route path='/verifyotp' element={<OtpVerify/>}/>
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
 
-=======
         <Route path='/view' element={ <KGHospitalInfo/>}/>
         <Route path='/view2' element={ <IMA/>}/>
         <Route path='/view3' element={ <Kuppu/>}/>
@@ -44,7 +39,6 @@ function AppRoutes() {
         <Route path='/hospitalview2' element={ <Corporation/>}/>
         <Route path='/hospitalview3' element={ <Port/>}/>
         <Route path='/hospital' element={ <Hospital/>}/>
->>>>>>> Vishali
       </Routes>
       <Footer />  
     </>
